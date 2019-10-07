2cfa3cc789eaffdcf98d167b83ecfa14ecc0da2e
zi-klon <zi-klon@yandex.ru>
2019-10-07 15:47:54 +0500

    Настроен hot-reload, хэширование, сборки dev и build

--- webpack.config.js
+++ webpack.config.js
@@ -10,60 +10,53 @@ module.exports = {
   entry: { main: './src/index.js' },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js'
+      filename: '[name].[chunkhash].js'
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: { loader: "babel-loader" }
-        },
-        {
-        test: /\.css$/i,
-        use: [
-              (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
-              'css-loader', 
-              'postcss-loader'
-            ]
-        },
-        {
-        test: /\.(png|jpe?g|gif|ico|svg)$/,
-        use: [
-               'file-loader?name=./images/[name].[ext]',
-                {
-                  loader: 'image-webpack-loader',
-                  options: {}
-                },
-            ],
-        },
-        {
-        test: /\.(eot|ttf|woff|woff2)$/,
-        loader: 'file-loader?name=./vendor/[name].[ext]'
-        }
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/, use: {loader: "babel-loader"}
+    },
+    {
+      test: /\.css$/i,
+      use:  [MiniCssExtractPlugin.loader, 'css-loader']
+    },
+    {
+      test: /\.(png|jpe?g|gif|ico|svg)$/,
+      use: 
+        [
+          'file-loader?name=./images/[name].[ext]',
+          {loader: 'image-webpack-loader', options: {}},
+        ],
+    },
+    {
+      test: /\.(eot|ttf|woff|woff2)$/,
+      loader: 'file-loader?name=./vendor/[name].[ext]'
+    }
     ]
-  },
+    },
     plugins: [
       new MiniCssExtractPlugin({
-        filename: 'style.[contenthash].css',
+      filename: 'style.[contenthash].css',
       }),
       new OptimizeCssAssetsPlugin({
         assetNameRegExp: /\.css$/g,
         cssProcessor: require('cssnano'),
         cssProcessorPluginOptions: {
-          preset: ['default'],
+        preset: ['default'],
         },
         canPrint: true
-    }),
+      }),
       new HtmlWebpackPlugin({
         inject: false,
         hash: true,
         template: './src/index.html',
         filename: 'index.html'
-    }),
+      }),
       new WebpackMd5Hash(),
       new webpack.DefinePlugin({
-      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })               
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      })               
   ]
 };
