dab94ed1eb4c0762bcaba06a8a6bd6b275a5d184
zi-klon <zi-klon@yandex.ru>
2019-10-14 16:10:53 +0500

    Перенастроена сборка для нескольких страниц, сверстаны блоки description и technologies без адаптива

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,14 @@ const isDev = process.env.NODE_ENV === 'development';
 const webpack = require('webpack');
 
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: {
+    'index': './src/index/index.js',
+    'about': './src/about/about.js',
+    'analytics': './src/analytics/analytics.js'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-      filename: '[name].[chunkhash].js'
+    filename: 'pages/[name]/[name].[chunkhash].js'
   },
   module: {
   rules: [
@@ -28,10 +32,9 @@ module.exports = {
     },
     {
       test: /\.(png|jpe?g|gif|ico|svg)$/,
-      use: 
-        [
+      use: [
           'file-loader?name=./images/[name].[ext]',
-          {loader: 'image-webpack-loader', options: {}},
+          { loader: 'image-webpack-loader', options: {}},
         ],
     },
     {
@@ -42,22 +45,34 @@ module.exports = {
     },
     plugins: [
       new MiniCssExtractPlugin({
-      filename: 'style.[contenthash].css',
+      filename: 'pages/[name]/[name].[contenthash].css',
       }),
       new OptimizeCssAssetsPlugin({
         assetNameRegExp: /\.css$/g,
         cssProcessor: require('cssnano'),
         cssProcessorPluginOptions: {
-        preset: ['default'],
+          preset: ['default'],
         },
         canPrint: true
       }),
       new HtmlWebpackPlugin({
         inject: false,
         hash: true,
-        template: './src/index.html',
+        template: './src/index/index.html',
         filename: 'index.html'
       }),
+      new HtmlWebpackPlugin({
+        inject: false,
+        hash: true,
+        template: './src/about/about.html',
+        filename: 'about.html'
+      }),
+      new HtmlWebpackPlugin({
+        inject: false,
+        hash: true,
+        template: './src/analytics/analytics.html',
+        filename: 'analytics.html'
+      }),    
       new WebpackMd5Hash(),
       new webpack.DefinePlugin({
         'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
